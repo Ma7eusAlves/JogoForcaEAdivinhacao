444cdf7ab1163b5bd299cb1c4364d8ff47362295
Mateus Alves <92250989+Ma7eusAlves@users.noreply.github.com>
2023-08-29 16:41:03 -0300

    Fazendo Upload dos Arquivos do Jogo

---
+++ jogos.py
@@ -0,0 +1,20 @@
+import forca
+import adivinhacao
+
+def escolhe_jogo():
+    print("*********************************")
+    print("*******Escolha o seu jogo!*******")
+    print("*********************************")
+
+    print("(1) Forca (2) Advinhação")
+    jogo = int(input("Qual jogo?"))
+
+    if(jogo == 1):
+        print("Jogando Forca")
+        forca.jogar()
+    elif(jogo == 2):
+        print("Jogando Adivinhação")
+        adivinhacao.Jogar()
+
+if (__name__ == "__main__"):
+    escolhe_jogo()
